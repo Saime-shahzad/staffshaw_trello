d4aaba19df0e7b5a0d19c5941d06f958123df6a6
Saime-shahzad <saimeshahzad7@gmail.com>
2025-01-16 20:53:27 +0500

    minor changes

--- src/assets/select/Popup.jsx
+++ src/assets/select/Popup.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import {  Popconfirm } from 'antd';
-import "./Popup.css"
+import React from "react";
+import { Popconfirm } from "antd";
+import "./Popup.css";
 
-const Popup = ({title , component , icon , className}) => (
+const Popup = ({ title, component, icon, className }) => (
   <Popconfirm
     title={title}
     description={component}
     icon={null}
-   trigger="click"
-   className="custom-popconfirm" 
-   
-    overlayStyle={{ width: '350px' }}
+    trigger="click"
+    className="custom-popconfirm"
+    overlayStyle={{ width: "350px" }}
   >
-    <button className= {className} danger>{icon}</button>
+    {/* Merge icon and title */}
+    <button className={className} danger style={{fontSize:"14px", color:"#172B4D", display: "flex", alignItems: "center", gap: "8px" }}>
+  {icon} {title}
+</button>
   </Popconfirm>
 );
+
 export default Popup;
